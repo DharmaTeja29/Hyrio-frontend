8987e866adbabb4bc04238243c2828917089086c
Dharma Teja <1234nelluridharma@gmail.com>
2025-01-31 00:11:42 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://hyrio-backend.onrender.com:10000',
+    baseURL: 'https://hyrio-backend.onrender.com',
     withCredentials: true
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: 'https://hyrio-backend.onrender.com:10000',
+    baseURL: 'https://hyrio-backend.onrender.com',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
